8541d25804cdc10acd2a9560b593b38af26c72db
simonebaldi <94689684+simonebaldi@users.noreply.github.com>
2021-11-20 20:37:13 +0800

    Add files via upload

---
+++ parameters/parameters_WP.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%% Mission Planner %%%%%%%%%%%%%%%%%%
+
+% MP.waypoints = load('waypoints.txt');
+
+Path.waypoints = [0 0;
+                500 500;
+                500 0;
+                0 500;
+                0 0;
+                500 500];            
